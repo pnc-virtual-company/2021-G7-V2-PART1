f34b6bb4af8887b3a8aebd87ed5566ed03793ad3
metchanthou <chanthoumet@email.com>
2021-12-02 11:19:39 +0700

    #27 manage category for back end

---
+++ back/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getAllCategory()
+    {
+        return Category::latest()->get();
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function createCategory(Request $request)
+    {
+        $category = new Category();
+        $category->categoryName = $request->categoryName;
+        $category->image = $request->image;
+        $category->save();
+
+        return response()->json(['message' => 'Create successfully!'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function categoryId($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateCategory(Request $request, $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->categoryName = $request->categoryName;
+        $category->image = $request->image;
+        $category->save();
+
+        return response()->json(['message' => 'Category update successfully!'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function deleteCategory($id)
+    {
+        $iSDelete = Category::destroy($id);
+        if ($iSDelete ===1) {
+            return response()->json(['message' => 'Category deleted successfully'], 200);
+        } else {
+            return response()->json(['message' => 'Category cannot delete'], 404);
+        }
+    }
+
+    /**
+     * search category name
+     *
+     * @param  int  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($categoryName)
+    {
+        return Category::where('categoryName','like','%'.$categoryName.'%')->get();
+    }
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,12 @@ Route::get('users/{id}', [UserController::class, 'getUserId']);
 // _____________________Private Route____________________
 Route::put('users/{id}', [UserController::class, 'updateUser']);
 Route::delete('users/{id}', [UserController::class, 'deleteUser']);
+
+
+// ____________________Category Route___________________
+Route::get('categories', [CategoryController::class, 'getAllCategory']);
+Route::get('categories/{id}', [CategoryController::class, 'categoryId']);
+Route::post('categories', [CategoryController::class, 'createCategory']);
+Route::put('categories/{id}', [CategoryController::class, 'updateCategory']);
+Route::delete('categories/{id}', [CategoryController::class, 'deleteCategory']);
+Route::get ('categories/search/{categoryName}',[CategoryController::class,'search']);
