171fb30ba161d293e52527762d58a7a503e40179
chanrychhoy <chanry.chhoy@student.passerellesnumeriques.org>
2021-12-10 09:05:58 +0700

    #00

--- front/src/main.js
+++ front/src/main.js
@@ -4,9 +4,9 @@ import router from './router/router.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 // 
-import BaseCard from './components/button/BaseCard.vue';
-import BaseButton from './components/button/BaseButton.vue';
-import BaseDialog from './components/button/BaseDialog.vue';
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
 
 const app = createApp(App);
 app.component('base-card', BaseCard);
